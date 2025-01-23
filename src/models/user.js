92f48501e42bc81bcc7855ec134ee33a69bb9ab0
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2025-01-23 22:58:35 +0630

    ADDING JWTTOEKN

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     username: {
@@ -40,12 +41,34 @@ Schema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     this.password = bcrypt.hash(this.password, 10);
   }
-  next(); 
+  next();
 });
 
-Schema.methods.isPasswordMatch = async function (password){
-  return await bcrypt.compare(password,this.password)
-}
+Schema.methods.isPasswordMatch = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+Schema.methods.generateAccessToken = async function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET_KEY,
+    { expiresIn: process.env.ACCESS_TOKEN_EXP_TIME }
+  );
+};
+
+Schema.methods.generateRefreshToken = async function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET_KEY,
+    { expiresIn: process.env.REFRESH_TOKEN_EXP_TIME }
+  );
+};
 
 userSchema.plugin(mongooseAggregatePaginate);
 
